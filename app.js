fa64708b3fab92149a745e9704399a7300e0337e
Queenie Lodonia <queenie.lodonia@webriq.services>
2018-10-30 14:22:31 +0800

    test

--- app.js
+++ app.js
@@ -2,11 +2,57 @@ const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
-const Collections = require('spike-collections')
 const Records = require('spike-records')
+const Collections = require('spike-collections')
+const excerpt = require('html-excerpt')
 const env = process.env.SPIKE_ENV
+const https = require('https')
+const fs = require('fs')
+const locals = {
+  dataExcerpt: postExcerpt
+}
+let request = require('request')
 
-const locals = {}
+function postExcerpt(html, length, ellipsis) {
+  return excerpt.text(html, length || 100, ellipsis || '...');
+}
+
+function getVideoThumbnail() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("data/site.json", (err, data) => {
+      if (err) throw err;
+      let obj = JSON.parse(data);
+      let videoId = obj.content[0].videoid;
+      const url = "" + videoId + ".json";
+      request.get(
+        "https://api.vimeo.com/videos/" + videoId, {
+          auth: {
+            bearer: "525b876a547a549ea9db0c236918d29b"
+          }
+        },
+        (e, r, body) => {
+          if (e) {
+            reject(e);
+            return;
+          }
+          let obj = JSON.parse(body);
+          let images = JSON.stringify(obj.pictures);
+          let items = JSON.parse(images);
+          for (let i = 0; i < items.sizes.length; i++) {
+            if (items.sizes[i].width >= 1024 && items.sizes[i].width < 1400) {
+              obj = {
+                title: "vimeo",
+                url: items.sizes[i].link
+              };
+              resolve(obj);
+              return;
+            }
+          }
+        }
+      );
+    });
+  });
+}
 const collections = new Collections({
   addDataTo: locals,
   collections: {
@@ -21,34 +67,23 @@ const collections = new Collections({
             return `posts/index.html`
           }
           return `posts/${n}.html`
-        } 
+        }
       }
     }
   }
 })
 const records = new Records({
   addDataTo: locals,
-  site: { file: 'data/site.json' }
-  // one: { file: 'data.json' },
-  // two: { url: 'http://api.carrotcreative.com/staff' },
-  // three: { data: { foo: 'bar' } },
-  // four: {
-  //   graphql: {
-  //     url: 'http://localhost:1234',
-  //     query: 'query { allPosts { title } }',
-  //     variables: 'xxx', // optional
-  //     headers: { authorization: 'Bearer xxx' } // optional
-  //   }
-  // },
-  // five: { callback: myFunc }
+  site: { file: "data/site.json" },
+  biography: { file: "data/biography.json" },
+  video: { callback: getVideoThumbnail }
 });
-
 module.exports = {
   devtool: 'source-map',
   ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
     locals: ctx => {
-      return collections.locals(ctx, Object.assign({ pageId: pageId(ctx) }, locals))
+      return ctx, Object.assign({ pageId: pageId(ctx) }, { deployVersion: new Date().getTime() }, locals)
     },
     minify: env === 'production'
   }),
@@ -56,5 +91,6 @@ module.exports = {
     minify: env === 'production'
   }),
   babel: jsStandards(),
-  plugins: [collections, records]
+  vendor: ['assets/js/**'],
+  plugins: [records, collections]
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,21 @@
-console.log('you can use ES6 here : )')
+$(function() {
+  $(".phone_number").text(function(i, text) {
+    text = text.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+    return text;
+  });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 63) /* .header-top height */ {
+      $('#menu').addClass('fixed');
+    } else {
+      $('#menu').removeClass('fixed');
+    }
+  });
+});
+
+
+$(document).ready(function(){
+
+  TweenMax.from('#slider-content span', 1.25, {opacity: "0", y: -20} )
+  TweenMax.from('#slider-content h1', 2, {x:30, opacity: "0", delay: 1.5})
+
+});
